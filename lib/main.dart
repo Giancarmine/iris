3352c2ab9b97873669ccc40a2a29190b2bb28d5a
carmine.cuofano <carmine.cuofano@zooplus.com>
2021-09-26 14:01:57 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:iris/screens/home_page.dart';
+import 'package:iris/services/theme_services.dart';
+import 'package:iris/utils/theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Iris',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeService().theme,
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:iris/services/notification_services.dart';
+import 'package:iris/services/theme_services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: const [
+          Text(
+            "Theme data",
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeService().switchTheme();
+          notifyHelper.displayNotification(
+              title: "Theme Changed",
+              body: Get.isDarkMode
+                  ? "Activated Light Theme"
+                  : "Activated Dark Theme");
+
+          notifyHelper.scheduledNotification();
+        },
+        child: const Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: const [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
