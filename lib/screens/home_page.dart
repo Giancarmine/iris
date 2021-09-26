0223c81ac48f960c85662dd98ad348261c409aef
carmine.cuofano <carmine.cuofano@zooplus.com>
2021-09-26 15:12:07 +0200

    Add date and add task

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:intl/intl.dart';
 import 'package:iris/services/notification_services.dart';
 import 'package:iris/services/theme_services.dart';
+import 'package:iris/utils/theme.dart';
+import 'package:iris/widgets/button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,13 +30,30 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: _appBar(),
       body: Column(
-        children: const [
-          Text(
-            "Theme data",
-            style: TextStyle(
-              fontSize: 30,
-            ),
-          ),
+        children: [
+          Row(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      DateFormat.yMMMMd().format(DateTime.now()),
+                      style: subHeadingStyle,
+                    ),
+                    Text(
+                      "Today",
+                      style: headingStyle,
+                    ),
+                  ],
+                ),
+              ),
+              MyButton(label: "+ Add Task", onTap: null),
+            ],
+          )
         ],
       ),
     );
@@ -41,6 +61,9 @@ class _HomePageState extends State<HomePage> {
 
   _appBar() {
     return AppBar(
+      //TODO: Fix context import
+      // backgroundColor: context.theme.backgroundColor,
+      elevation: 0,
       leading: GestureDetector(
         onTap: () {
           ThemeService().switchTheme();
@@ -52,17 +75,19 @@ class _HomePageState extends State<HomePage> {
 
           notifyHelper.scheduledNotification();
         },
-        child: const Icon(
-          Icons.nightlight_round,
+        child: Icon(
+          Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round,
           size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
         ),
       ),
-      actions: const [
+      actions: [
         Icon(
-          Icons.person,
+          Icons.person_pin,
           size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
         ),
-        SizedBox(
+        const SizedBox(
           width: 20,
         ),
       ],
